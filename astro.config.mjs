a6d74305e3d0e4436b26871408b0832700f24866
Steve Wolfe <stevenwolfe@gmail.com>
2026-05-12 21:45:46 -0400

    link fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://anchorspec.github.io',
-  base: '/www',
+  site: 'https://www.anchorspec.com',
 });
